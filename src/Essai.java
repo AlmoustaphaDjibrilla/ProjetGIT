adb8986cfa1d9c3afe4a1a8a5c4fe62b6b7fe42e
AlmoustaphaDjibrilla <almoustaphadjibrillaidrissa@gmail.com>
2022-10-05 12:55:34 +0200

    Nouveau commit pour tester la somme des entiers

--- src/Essai.java
+++ src/Essai.java
@@ -4,5 +4,11 @@ public class Essai {
 	public static void main (String[] args) {
 		
 		System.out.println("Bonjour à tous");
+		
+		int i=1, j=2, k=3;
+		
+		int somme= (int) (i+j+k);
+		
+		System.out.println ("La somme de "+i+" , de "+j+" et de "+k+" est: "+somme);
 	}
 }
