7ffe13b60ebf7e243c562ace10a47b3b134c541b
Appi72020 <abdoulraouf72020@gmail.com>
2022-10-05 16:53:58 +0100

    Nouveau commit fait par abdoul Raouf

--- src/Essai.java
+++ src/Essai.java
@@ -3,12 +3,16 @@ public class Essai {
 
 	public static void main (String[] args) {
 		
-		System.out.println("Bonjour à tous");
+		System.out.println("Bonjour ï¿½ tous");
 		
 		int i=1, j=2, k=3;
 		
 		int somme= (int) (i+j+k);
 		
 		System.out.println ("La somme de "+i+" , de "+j+" et de "+k+" est: "+somme);
+
+		String message="\n\n\tCeci est une nouvelle modification apportÃ©e par Abdoul Raouf depuis son ordinateur portable\n\n";
+
+		System.out.println (message);
 	}
 }
